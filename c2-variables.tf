c6e6faddcb2ce1c576fd04d90fb92aa872406212
drrunix <drrunix@gmail.com>
2021-07-20 23:39:06 -0400

    Secound count commit

--- c2-variables.tf
+++ c2-variables.tf
@@ -2,11 +2,10 @@
 variable "aws_region" {
   description = "Region in which AWS Resources to be created"
   type = string
-  default = "us-east-2"
-}
+  }
 
 variable "instance_type" {
   description = "EC2 Instance Type - Instance Sizing"
   type = string
-  default = "t2.small"
+  
 }
